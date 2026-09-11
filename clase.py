38be15be8b390f8bdd3b54afaed0a0bb08f32651
paola1312 <mosquerapaola276@gmail.com>
2026-09-10 19:26:36 -0500

    mi primera clase

---
+++ clase.py
@@ -0,0 +1,17 @@
+# String
+palabra = "Hola mundo"
+
+# Int - Integer
+porcentaje = 100
+
+# float
+nota = 3.0
+
+
+agrupacion = palabra + "soy Paola tengo la bateria al " +str(porcentaje) + " y mi nota hoy es " + str(nota)
+
+print ( agrupacion)
+
+
+#Hola mundo
+# Hola mundo soy Paola tengo la bateria al 100 y mi nota de hoy es 3.0
---
+++ tarea1.py
@@ -0,0 +1,22 @@
+# 3 variables para la siguiente oración
+# Me llamo Funalo 
+# tengo 8 años
+# y tengo 2 papás
+# 1 variable con texto y 2 variables con enteros 
+# se debe imprimir, con sus respectivos espacios
+
+
+
+
+nombre = "Fulano"
+
+#Int
+edad = 8
+
+#Int
+padres = 2 
+
+
+agrupacion = " Me llamo " + nombre + " tengo " + str(edad) + " años y tengo " + str(padres) + " papás "
+
+print (agrupacion)
